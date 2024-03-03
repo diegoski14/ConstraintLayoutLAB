23c81913b031b186ba9ff447460a0bff5a61de29
diegoski14 <diegosc1920@gmail.com>
2024-03-02 20:30:11 -0500

    1er commit

---
+++ app/src/main/java/co/edu/unipiloto/constraintlayoutlab/MainActivity.java
@@ -0,0 +1,26 @@
+package co.edu.unipiloto.constraintlayoutlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import java.util.Properties;
+import javax.mail.*;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickSend (View view){
+
+
+
+
+
+        }
+    }
+
+
+
